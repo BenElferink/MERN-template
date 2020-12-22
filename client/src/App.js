6c90e8dc284a0af3d04470270ead8bf2e1292223
Ben Elferink <ben.elferink@icloud.com>
2020-12-23 00:54:43 +0200

    api base structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './style/style.css';
+// import * as api from './api';
+// un-comment this ^ ^ ^ to import api endpoints
 
 function App() {
+  // React.useEffect(() => {
+  //   api
+  //     .getSomething() // replace this with your endpoint
+  //     .then((response) => console.log(`✅ ${response.status} ${response.statusText}`, response.data))
+  //     .catch((error) => console.log(`❌ ${error}`));
+  // });
+  //
+  // example ^ ^ ^ using the api endpoint
+
   return <div>Hello World - React.js</div>;
 }
 
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+// customizable base-url
 const url = 'http://localhost:8080/';
 
+// demonstration endpoints:
+//
 // export const getSomething = () => axios.get(url + 'path');
 // export const postSometing = (form) => axios.post(url + 'path', form);
