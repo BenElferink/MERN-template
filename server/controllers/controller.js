3dff3e6b963f46b1d6efacaa48367402bcb55cd8
Ben Elferink <ben.elferink@icloud.com>
2020-12-23 15:44:35 +0200

    Simplified some things

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,16 +2,15 @@ import Example from './../models/model.js';
 
 const getExamples = (request, response) => {
   // what is .find() ???   --->   https://mongoosejs.com/docs/queries.html
-  Example.find((error, allExamples) => {
+  Example.find((error, data) => {
     if (error) {
       console.log(`❌ ${error}`);
       return response.status(404).json(error); // 404 not found + catched error -> send to client
+    } else {
+      console.log('✅ -FOUND- :', data);
+      return response.status(200).json(data); // 200 ok + array of documents -> send to client
     }
-
     // more about response status codes   --->   https://restapitutorial.com/httpstatuscodes.html
-
-    console.log('✅ -FOUND- :', allExamples);
-    return response.status(200).json(allExamples); // 200 ok + array of couments -> send to client
   });
 };
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import getExamples from './../controllers/controller.js'; // import request & response function
+
 const router = express.Router(); // initialize router
 
 /*
   1st param = extended path location
   2nd param = request & response function
-
-  current link: http://localhost:8080/examples
 */
-router.get('/examples', getExamples);
+router.get('/examples', getExamples); // current path: http://localhost:8080/examples
 
 export default router;
