1d481a17ae65e4785e2db4aaa7ee15bd5c5c3b10
Ben Elferink <ben.elferink@icloud.com>
2020-12-25 19:18:56 +0200

    added new post example route

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import getExamples from './../controllers/controller.js'; // import request & response function
+import { getExamples, uploadExample } from './../controllers/controller.js'; // import request & response function
 
 const router = express.Router(); // initialize router
 
@@ -7,6 +7,7 @@ const router = express.Router(); // initialize router
   1st param = extended path location
   2nd param = request & response function
 */
-router.get('/examples', getExamples); // current path: http://localhost:8080/examples
+router.get('/', getExamples); // current path: http://localhost:8080/example
+router.post('/upload', uploadExample); // current path: http://localhost:8080/example/upload
 
 export default router;
