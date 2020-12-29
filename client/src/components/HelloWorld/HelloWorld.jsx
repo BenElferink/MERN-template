1c9a390ee79b2286efa29cd02613aac24ce058ac
Ben Elferink <ben.elferink@icloud.com>
2020-12-29 19:44:50 +0200

    deleted folder

--- client/src/components/HelloWorld/HelloWorld.jsx
+++ client/src/components/HelloWorld/HelloWorld.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './style/HelloWorld.module.css'; // CSS module -> className={styles.App} (AKA styled components)
+import styles from './HelloWorld.module.css'; // CSS module -> className={styles.App} (AKA styled components)
 
 function HelloWorld() {
   return <div className={styles.HelloWorld}>Hello World - React.js</div>;
