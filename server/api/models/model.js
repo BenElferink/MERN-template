833ac944c36f4f370c4ba832e29c882312d2002a
Ben Elferink <69640911+belferink1996@users.noreply.github.com>
2020-12-29 17:53:47 +0200

    added timestamps to example Schema

--- server/api/models/model.js
+++ server/api/models/model.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 
-const instance = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // _id is set by default, (you can remove this line)
-  /*
+const instance = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId, // _id is set by default, (you can remove this line)
+    /*
     name = property of document object
     String = type of value   --->   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
   */
-  name: String,
-});
+    name: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
 // document = model name   --->   https://mongoosejs.com/docs/guide.html
 // note: use a singular name, mongoose automatically creates a collection like so -> model: 'Person' === collection: 'people'
