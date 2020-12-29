45079031575ee30ea213b2a18bcfeacc9ebcc1b6
Ben Elferink <ben.elferink@icloud.com>
2020-12-29 19:42:12 +0200

    implemented reset stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style.reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
