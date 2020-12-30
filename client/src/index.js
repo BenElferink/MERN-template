be3e2892d2bad1ec3cb77f78893dd867aa615095
Ben Elferink <69640911+belferink1996@users.noreply.github.com>
2020-12-30 17:20:20 +0200

    typo

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style.reset.css';
+import './style/reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
