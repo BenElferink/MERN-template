c2b5e7bccdaaf81825b3cc2e6d9f541aacc14574
Ben Elferink <69640911+belferink1996@users.noreply.github.com>
2020-12-30 19:09:52 +0200

    Update model.js

--- server/api/models/model.js
+++ server/api/models/model.js
@@ -4,9 +4,9 @@ const instance = new mongoose.Schema(
   {
     _id: mongoose.Schema.Types.ObjectId, // _id is set by default, (you can remove this line)
     /*
-    name = property of document object
-    String = type of value   --->   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
-  */
+      name = property of document object
+      String = type of value   --->   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+    */
     name: String,
   },
   {
@@ -16,6 +16,6 @@ const instance = new mongoose.Schema(
 
 // document = model name   --->   https://mongoosejs.com/docs/guide.html
 // note: use a singular name, mongoose automatically creates a collection like so -> model: 'Person' === collection: 'people'
-const document = 'Example';
+const modelName = 'Example';
 
-export default mongoose.model(document, instance);
+export default mongoose.model(modelName, instance);
