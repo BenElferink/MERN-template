a205f496c9d94d62f4c38fd6e2a37ce27a69ddc5
Ben Elferink <69640911+belferink1996@users.noreply.github.com>
2020-12-31 03:21:39 +0200

    Update model.js

--- server/api/models/model.js
+++ server/api/models/model.js
@@ -14,7 +14,7 @@ const instance = new mongoose.Schema(
   },
 );
 
-// document = model name   --->   https://mongoosejs.com/docs/guide.html
+// modelName   --->   https://mongoosejs.com/docs/guide.html
 // note: use a singular name, mongoose automatically creates a collection like so -> model: 'Person' === collection: 'people'
 const modelName = 'Example';
 
