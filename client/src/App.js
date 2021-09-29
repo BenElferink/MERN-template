1c17915ea28f4f1c30f10b87f006adbae7791e10
Ben Elferink <69640911+belferink1996@users.noreply.github.com>
2021-09-29 11:01:42 +0300

    Delete client directory

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style/style.css';
-
-function App() {
-  return <div>Hello World - React.js</div>;
-}
-
-export default App;
--- client/src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:8080/';
-
-// export const getSomething = () => axios.get(url + 'path');
-// export const postSometing = (form) => axios.post(url + 'path', form);
