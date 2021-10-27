1a841acaa26c94055b19120253640e7c11c6c6e6
Ben Elferink <ben.elferink@icloud.com>
2021-10-27 19:31:01 +0300

    fix: friendly syntax

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,5 @@
-const {
-  // api url (where your server is hosted at)
-  REACT_APP_BACKEND_URL: BACKEND_URL = 'http://localhost:8080',
-} = process.env
+// api url (where your server is hosted at)
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080'
 
 export {
   BACKEND_URL,
